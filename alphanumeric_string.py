5daf1f934f149a84eadf3d0d6966b196ed7179d0
draken <shindeprajwal11116@gmail.com>
2023-07-17 17:41:41 +0530

    Done

---
+++ alphanumeric_string.py
@@ -0,0 +1,8 @@
+import random
+import string
+def Demo():
+    a="".join(random.choices(string.digits + string.ascii_lowercase , k=7))
+    print(a)
+   # for i in range(0,len(a)):
+      #  if type(i)==
+Demo()
